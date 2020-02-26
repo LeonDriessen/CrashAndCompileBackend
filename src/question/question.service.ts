239fd26d3c85d7887adb507bffc641a4fcf6f4c7
unknown <driessen.leon@gmail.com>
2020-02-26 15:43:20 +0100

    insert, delete and patch

--- src/question/question.service.ts
+++ src/question/question.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -18,5 +18,65 @@ export class QuestionService {
       answer: quest.answer,
       points: quest.points,
     }));
+  }
+  async insertQuestion(question: string, answer: string, points: number) {
+    const newQuestion = new this.questionModel({
+      question,
+      answer,
+      points,
+    });
+    const result = await newQuestion.save();
+    return result.id as string;
+  }
+
+  async getSingleQuestion(questionId: string) {
+    const question = await this.findQuestion(questionId);
+    return {
+      id: question.id,
+      question: question.question,
+      answer: question.answer,
+      points: question.points,
+    };
+  }
+
+  async deleteProduct(questionId: string) {
+    const result = await this.questionModel
+      .deleteOne({ _id: questionId })
+      .exec();
+    if (result.n === 0) {
+      throw new NotFoundException('Could not find product to delete.');
+    }
+  }
+
+  async updateQuestion(
+    questionId: string,
+    question: string,
+    answer: string,
+    points: number,
+  ) {
+    const updatedQuestion = await this.findQuestion(questionId);
+    if (question) {
+      updatedQuestion.question = question;
+    }
+    if (answer) {
+      updatedQuestion.answer = answer;
+    }
+    if (points) {
+      updatedQuestion.points = points;
+    }
+    updatedQuestion.save();
+  }
+
+  private async findQuestion(id: string): Promise<Question> {
+    let question;
+    try {
+      question = await this.questionModel.findById(id).exec();
+    } catch (error) {
+      throw new NotFoundException('Could not find the question.');
+    }
+    if (!question) {
+      throw new NotFoundException('Could not find the question.');
+    }
+    return question;
   }
 }
